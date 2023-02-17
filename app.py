a8cdd0de093188673c137cffa5a55867c8dc1440
Moder devs <108572026+Sam-k-migz@users.noreply.github.com>
2023-02-17 14:58:06 +0000

    connect to dynamic data

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS =[
+{
+  'id':1,
+  'location': 'Nairobi',
+  'title': 'Software developer',
+  'salary': '40,000ksh'
+},
+{
+  'id':2,
+  'location': 'Eldoret',
+  'title': 'Data Scientist',
+  'salary': '80,000ksh'
+},
+{
+  'id':1,
+  'location': 'Mombasa',
+  'title': 'Software developer',
+  'salary': '40,000ksh'
+},
+{
+  'id':1,
+  'location': 'Nairobi',
+   'title': 'Frontend developer',
+  'salary': '30,000ksh'
+},
+]
+
 @app.route("/")
 
-def sayHellp():
-  return "hello world"
+def sayHello():
+  return render_template('home.html',jobs=JOBS)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
